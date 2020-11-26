07ba6fdd61de38b58df43229bfb0e0611bf6593d
王先生 <839462980@qq.com>
2020-11-26 20:42:35 +0800

    3

--- src/main/java/com/myspringboot/controller/userController.java
+++ src/main/java/com/myspringboot/controller/userController.java
@@ -50,3 +50,22 @@
 //
 //
 //}
+
+
+import com.myspringboot.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("userController")
+public class userController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+
+
+}
