f458843e6af381aabe4b556573967d8965629f8e
王先生 <839462980@qq.com>
2020-11-30 15:55:41 +0800

    4

--- src/main/java/com/myspringboot/controller/userController.java
+++ src/main/java/com/myspringboot/controller/userController.java
@@ -54,7 +54,6 @@
 
 import com.myspringboot.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
